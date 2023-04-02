9196e6fec6c5c7933f93fd29052deb910ded33a6
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-02 23:41:30 +0530

    updating

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ module "mongodb" {
   source               = "./vendor/modules/mongodb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
 }
+
+module "redis" {
+  source               = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                  = var.ENV
+}
