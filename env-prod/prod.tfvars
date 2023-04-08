ad5edaa6917b3be70fad53de4ad2cea3914c182b
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-08 11:13:33 +0530

    updating

--- env-prod/prod.tfvars
+++ env-prod/prod.tfvars
@@ -1,1 +1,22 @@
 ENV                     = "prod"
+
+# MySQL Variable Values 
+RDS_MYSQL_PORT = 3306
+RDS_MYSQL_STORAGE = 10
+RDS_MYSQL_ENGINE_VERSION = "5.7"
+RDS_INSTANCE_TYPE = "db.t3.micro"
+
+# DocDB Values
+DOCDB_PORT = 27017
+DOCDB_INSTANCE_CLASS = "db.t3.medium"
+DOCDB_INSTANCE_COUNT = 1
+
+# Elastic Cache Value
+REDIS_PORT = 6379
+REDIS_INSTANCE_NODETYPE = "cache.t3.micro"
+REDIS_INSTANCE_COUNT = 1
+REDIS_ENGINE_VERSION = "6.x"
+
+# RabbitMQ 
+
+# RABBITMQ_PORT = "5672"
--- main.tf
+++ main.tf
@@ -1,14 +1,54 @@
+# module "mongodb" {
+#   source               = "./vendor/modules/mongodb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+#   ENV                  = var.ENV
+# }
+
+# module "redis" {
+#   source               = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+#   ENV                  = var.ENV
+# }
+
+# module "mysql" {
+#   source               = "./vendor/modules/mysql"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+#   ENV                  = var.ENV
+# }
+
 module "mongodb" {
   source               = "./vendor/modules/mongodb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
+  DOCDB_PORT           = var.DOCDB_PORT
+  DOCDB_INSTANCE_CLASS = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT = var.DOCDB_INSTANCE_COUNT
+
 }
 
 module "redis" {
-  source               = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
-  ENV                  = var.ENV
+  source                  = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                     = var.ENV
+  REDIS_PORT              = var.REDIS_PORT
+  REDIS_INSTANCE_NODETYPE = var.REDIS_INSTANCE_NODETYPE
+  REDIS_INSTANCE_COUNT    = var.REDIS_INSTANCE_COUNT
+  REDIS_ENGINE_VERSION    = var.REDIS_ENGINE_VERSION
+
 }
 
 module "mysql" {
   source               = "./vendor/modules/mysql"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
+  RDS_MYSQL_PORT       = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE    = var.RDS_MYSQL_STORAGE
+  RDS_MYSQL_ENGINE_VERSION = var.RDS_MYSQL_ENGINE_VERSION
+  RDS_INSTANCE_TYPE    = var.RDS_INSTANCE_TYPE
 }
+
+# # Provisioning RabbitMQ on EC2
+# module "rabbitmq" {
+#   source               = "./vendor/modules/rabbitmq"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+#   ENV                  = var.ENV
+#   RABBITMQ_PORT        = var.RABBITMQ_PORT
+# }
+
+# # Just for debugging
+# output "MONGO_ENDPOINT" {
+#     value = module.mongodb.MONGO_ENDPOINT
+# }
